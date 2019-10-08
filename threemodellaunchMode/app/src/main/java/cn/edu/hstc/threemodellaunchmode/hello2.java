532c637151902523a2ee4cd74a7af4d7848aada0
xhd123456 <2017118140@stu.hstc.edu.cn>
2019-10-08 15:34:31 +0800

    活动的四种模式：standard、singleTop、singleTask、singleInstance

---
+++ threemodellaunchMode/app/src/main/java/cn/edu/hstc/threemodellaunchmode/hello2.java
@@ -0,0 +1,78 @@
+package cn.edu.hstc.threemodellaunchmode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class hello2 extends AppCompatActivity {
+
+    Button bt1,bt2,bt3;
+    public static final String TAG="hello2";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello2);
+        bt1=(Button) findViewById(R.id.btTohello1);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            /*Intent intent=new Intent();
+            intent.setClass(hello1.this,hello2.class);*/
+                Intent intent=new Intent(hello2.this, hello1.class);
+                startActivity(intent);
+            }
+        });
+        bt2=(Button) findViewById(R.id.btTohello2);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            /*Intent intent=new Intent();
+            intent.setClass(hello1.this,hello2.class);*/
+                Intent intent=new Intent(hello2.this, hello2.class);
+                startActivity(intent);
+            }
+        });
+        setTitle("欢迎来到hello2");
+        bt3=(Button) findViewById(R.id.btTohello3);
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(hello2.this,hello3.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart execute");
+    }
+
+    @Override
+    protected void onResume() { //恢复
+        super.onResume();
+        Log.d(TAG, "onResume execute");
+    }
+
+    @Override
+    protected void onPause() { //暂停
+        super.onPause();
+        Log.d(TAG, "onPause execute");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop execute");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy execute");
+    }
+}
